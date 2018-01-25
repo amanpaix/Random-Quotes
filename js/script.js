d4469c955cc0bd26b2bc13d8e811a95bd6258fb3
Aman <usernameispaix@gmail.com>
2018-01-25 19:32:19 +0530

    Pushing from local computer to Github

---
+++ js/script.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+  function genrateQuote() {
+    $.getJSON("../API/quote.json", function(json) {
+
+      var newQuote = Math.floor((Math.random() * json.length));
+      $("#message").html("&ldquo;" + json[newQuote].quote + "&rdquo;").fadeOut(0).fadeIn(2000);
+      $("#author").html(json[newQuote].author).fadeOut(0).fadeIn(2000);
+      $("#by").fadeOut(0).fadeIn(2000);
+      console.log(json[newQuote].quote);
+      console.log(json[newQuote].author);
+    });
+  }
+
+  function tweet() {
+    var tweetContent = $("#message").html() + " #quote";
+    var tweetLink = "https://twitter.com/home?status=" + encodeURIComponent(tweetContent);
+    window.open(tweetLink , "_blank");
+    console.log(tweetContent);
+  }
+
+  function post() {
+
+  }
+
+  genrateQuote();
+
+  $(".qbtn").on("click" , genrateQuote);
+  $(".tbtn").on("click" , tweet);
+  // $(".fbtn").on("click" , post);
+});
